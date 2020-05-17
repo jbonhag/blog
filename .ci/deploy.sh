9fa8fd05cda09d3fcca9b58b8d36e497483ff2d7
Jeff Bonhag <jeff@thebonhags.com>
2020-05-17 16:15:45 -0400

    tweak deploy action

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-# fetch the existing site
+# prepare environment
+bundle install || exit 1
+
+# rebuild the site
+bundle exec jekyll build
+
+# commit changes
 if [ -d /tmp/jbonhag.github.io ]; then
   /bin/rm -fr /tmp/jbonhag.github.io
 fi
 git clone git@github.com:jbonhag/jbonhag.github.io /tmp/jbonhag.github.io
-
-# rebuild the site
-jekyll build
-
-# commit changes
 rsync -azv --delete --exclude=.git /Users/jeff/Documents/blog/_site/ /tmp/jbonhag.github.io
 pushd /tmp/jbonhag.github.io
 git add .
